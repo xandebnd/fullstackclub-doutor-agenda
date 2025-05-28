40b0450fe9279caf7caa7f3a8108452a27e6f32e
Alexandre Bandeira <xandebnd@hotmail.com>
2025-05-28 12:26:00 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { Button } from "../components/ui/button";
+
 export default function Home() {
-  return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+  return <Button>Bootcamp</Button>;
 }

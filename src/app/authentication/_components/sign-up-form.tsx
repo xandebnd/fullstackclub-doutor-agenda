ec15aa0271e5e0403e62de9b8c65ff7d8b10eff3
Alexandre Bandeira <xandebnd@hotmail.com>
2025-05-29 10:59:00 -0300

    feat: add custom error message for existing user

--- src/app/authentication/_components/sign-up-form.tsx
+++ src/app/authentication/_components/sign-up-form.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "../../../components/ui/button";
@@ -61,6 +62,14 @@ const SignUpForm = () => {
       {
         onSuccess: () => {
           router.push("/dashboard");
+        },
+        onError: (ctx) => {
+          if (ctx.error.code === "USER_ALREADY_EXISTS") {
+            toast.error("E-mail já cadastrado");
+            return;
+          }
+
+          toast.error("Erro ao criar conta");
         },
       },
     );

b3f1d46a6ed826fd5752f5f72757a2aad312f96b
Alexandre Bandeira <xandebnd@hotmail.com>
2025-05-29 11:13:22 -0300

    feat: implement authentication flow with session handling and sign-out functionality

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,29 +1,41 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 import {
-	Tabs,
-	TabsContent,
-	TabsList,
-	TabsTrigger,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
 } from "../../components/ui/tabs";
+import { auth } from "../../lib/auth";
 import LoginForm from "./_components/login-form";
 import SignUpForm from "./_components/sign-up-form";
 
-const AuthenticationPage = () => {
-	return (
-		<div className="flex h-screen w-screen items-center justify-center">
-			<Tabs defaultValue="login" className="w-[400px]">
-				<TabsList className="grid w-full grid-cols-2">
-					<TabsTrigger value="login">Login</TabsTrigger>
-					<TabsTrigger value="register">Criar conta</TabsTrigger>
-				</TabsList>
-				<TabsContent value="login">
-					<LoginForm />
-				</TabsContent>
-				<TabsContent value="register">
-					<SignUpForm />
-				</TabsContent>
-			</Tabs>
-		</div>
-	);
+const AuthenticationPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Criar conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <LoginForm />
+        </TabsContent>
+        <TabsContent value="register">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default AuthenticationPage;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,24 @@
-const DashboardPage = () => {
-	return <div>teste</div>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "../../lib/auth";
+import SignOutButton from "./_components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  return (
+    <div>
+      {JSON.stringify(session?.user)}
+      <SignOutButton />
+    </div>
+  );
 };
 
 export default DashboardPage;

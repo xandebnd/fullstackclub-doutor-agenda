f6768d7d94e2b4702976d4faaf483bd290b71ad0
Alexandre Bandeira <xandebnd@hotmail.com>
2025-05-29 15:04:23 -0300

    feat: add sign in with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,24 +6,30 @@ import * as schema from "@/db/schema";
 import { db } from "../db";
 
 export const auth = betterAuth({
-	database: drizzleAdapter(db, {
-		provider: "pg",
-		usePlural: true,
-		schema,
-	}),
-	user: {
-		modelName: "usersTable",
-	},
-	session: {
-		modelName: "sessionsTable",
-	},
-	account: {
-		modelName: "accountsTable",
-	},
-	verification: {
-		modelName: "verificationsTable",
-	},
-	emailAndPassword: {
-		enabled: true,
-	},
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+    schema,
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  user: {
+    modelName: "usersTable",
+  },
+  session: {
+    modelName: "sessionsTable",
+  },
+  account: {
+    modelName: "accountsTable",
+  },
+  verification: {
+    modelName: "verificationsTable",
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
 });

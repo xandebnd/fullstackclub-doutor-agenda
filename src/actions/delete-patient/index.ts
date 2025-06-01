a95c52647b71f72337208c90b61bc3190c6b75ae
Alexandre Bandeira <xandebnd@hotmail.com>
2025-05-31 22:46:16 -0300

    feat: ensure patient can be deleted

--- src/actions/delete-patient/index.ts
+++ src/actions/delete-patient/index.ts
@@ -20,18 +20,24 @@ export const deletePatient = actionClient
 		const session = await auth.api.getSession({
 			headers: await headers(),
 		});
+
 		if (!session?.user) {
 			throw new Error("Unauthorized");
 		}
+
 		const patient = await db.query.patientsTable.findFirst({
 			where: eq(patientsTable.id, parsedInput.id),
 		});
+
 		if (!patient) {
 			throw new Error("Paciente não encontrado");
 		}
+
 		if (patient.clinicId !== session.user.clinic?.id) {
 			throw new Error("Paciente não encontrado");
 		}
+
 		await db.delete(patientsTable).where(eq(patientsTable.id, parsedInput.id));
+
 		revalidatePath("/patients");
 	});

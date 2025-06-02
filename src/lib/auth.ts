974eac0310a7d31a95f02c222ee48f9e88cdda81
Alexandre Bandeira <xandebnd@hotmail.com>
2025-06-02 12:04:23 -0300

    feat: add stripe webhook processing

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -25,6 +25,7 @@ export const auth = betterAuth({
         where: eq(schema.usersToClinicsTable.userId, user.id),
         with: {
           clinic: true,
+          user: true,
         },
       });
 
@@ -33,6 +34,7 @@ export const auth = betterAuth({
       return {
         user: {
           ...user,
+          plan: clinic.user.plan,
           clinic: clinic.clinicId
             ? {
                 id: clinic?.clinicId,
@@ -46,6 +48,23 @@ export const auth = betterAuth({
   ],
   user: {
     modelName: "usersTable",
+    additionalFields: {
+      stripeCustomerId: {
+        type: "string",
+        fieldName: "stripeCustomerId",
+        required: false,
+      },
+      stripeSubscriptionId: {
+        type: "string",
+        fieldName: "stripeSubscriptionId",
+        required: false,
+      },
+      plan: {
+        type: "string",
+        fieldName: "plan",
+        required: false,
+      },
+    },
   },
   session: {
     modelName: "sessionsTable",

6b85d849603174553f3cad7e805b05ea9c9acd99
Alexandre Bandeira <xandebnd@hotmail.com>
2025-06-02 21:36:34 -0300

    feat: redirect user to dashboard on home page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { auth } from "../lib/auth";
-
 export const Home = async () => {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  if (!session?.user) {
-    redirect("/authentication");
-  } else {
-    redirect("/dashboard");
-  }
+  redirect("/dashboard");
 };
